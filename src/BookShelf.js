90d2c2d64c85712c3f006b4dae00a13edeb2636f
Biyun Wu <beenwu2016@gmail.com>
2018-08-02 13:09:35 -0400

    Show selected option in the <select> element according to book obj's 'shelf' value

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,39 +1,37 @@
 import React, { Component } from 'react'
 
-class BookShelf extends Component {
+export default class BookShelf extends Component {
+    checkOption = (optionName, book) => optionName === book.shelf? 'selcted' : ''
+    
     render() {
         return (
             <div className="bookshelf">
                 <h2 className="bookshelf-title">{this.props.shelf}</h2>
                 <div className="bookshelf-books">
                     <ol className="books-grid">
-                        {
-                            this.props.books.map(book => 
-                                <li key={book.id}>
-                                    <div className="book">
-                                        <div className="book-top">
-                                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})`}}></div>
-                                            <div className="book-shelf-changer">
-                                                <select>
+                        {this.props.books.map(book => 
+                            <li key={book.id}>
+                                <div className="book">
+                                    <div className="book-top">
+                                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})`}}></div>
+                                        <div className="book-shelf-changer">
+                                            <select>
                                                 <option value="move" disabled>Move to...</option>
-                                                <option value="currentlyReading">Currently Reading</option>
-                                                <option value="wantToRead">Want to Read</option>
-                                                <option value="read">Read</option>
-                                                <option value="none">None</option>
-                                                </select>
-                                            </div>
+                                                <option value="currentlyReading" selected={this.checkOption("currentlyReading", book)}>Currently Reading</option>
+                                                <option value="wantToRead" selected={this.checkOption("wantToRead", book)}>Want to Read</option>
+                                                <option value="read" selected={this.checkOption("read", book)}>Read</option>
+                                                <option value="none" selected={this.checkOption("none", book)}>None</option> 
+                                            </select>
                                         </div>
-                                        <div className="book-title">{book.title}</div>
-                                        <div className="book-authors">{book.authors.join(", ")}</div>
                                     </div>
-                                </li>
-                            )
-                        }
+                                    <div className="book-title">{book.title}</div>
+                                    <div className="book-authors">{book.authors.join(", ")}</div>
+                                </div>
+                            </li>
+                        )}
                     </ol>
                 </div>
             </div>
         )
     }
 }
-
-export default BookShelf
